fa3d6f11b2d91743044e59a396a637a75cddf212
Marmex Mander <vadim.m.lutsk@gmail.com>
2024-10-31 13:33:49 +0200

    Purged Defaults.cs

--- Defaults.cs
+++ Defaults.cs
@@ -3,9 +3,5 @@ namespace Creators;
 
 public static class Defaults
 {
-    public static readonly Media DefaultPFP = new (){
-        Guid = Guid.Parse("d13c0dee-d9a1-4309-974a-487385f6a284"),
-        Author = "System",
-        OriginalName = "defaultpfp.png",
-    };
+    
 }
