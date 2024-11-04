b647db0ee0b821ea0a2bb1655cac2bbdae5f1e45
Marmex Mander <vadim.m.lutsk@gmail.com>
2024-11-04 20:24:24 +0200

    Added tag selector component + API

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Creators.Models;
+using Creators.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Creators.Controllers
+{
+    [Route("api/{action}")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        CreatorsDbContext _dbContext;
+
+        public ApiController(CreatorsDbContext creatorsDbContext)
+        {
+            _dbContext = creatorsDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> SearchTags(string query, int max = 100)
+        {
+            var tags = await _dbContext.Tags
+            .AsNoTracking()
+            .Where(t => t.Name.Contains(query))
+            .Take(max)
+            .Select(t => t.Name)
+            .ToListAsync();
+
+            return tags;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<TagInfo>> GetTagInfo(string tagName)
+        {
+            var tag = await _dbContext.Tags
+            .Include(t=>t.Info)
+            .FirstAsync(t=>t.Name == tagName);
+        
+            return tag.Info;
+        }
+        
+    }
+}
