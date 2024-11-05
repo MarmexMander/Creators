dcf5c4f8825e14b51669803101b7dcb815d65fc8
Marmex Mander <vadim.m.lutsk@gmail.com>
2024-11-05 18:50:39 +0200

    Added labels to Publication\Add

--- Areas/Gallery/Models/PublicationDTO.cs
+++ Areas/Gallery/Models/PublicationDTO.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using  Creators.Models;
 public class PublicationDTO
 {
+    [DisplayName("Publicaaation Title")]
     public string Title { get; set; }
+    [DisplayName("Write your work here")]
     public string? TextContent {get;set;}
+    [DisplayName("Description")]
     public string? Description { get; set;}
+    [DisplayName("Upload your work here")]
     public IFormFile Media { get; set; }
+    [DisplayName("Category")]
     public CategoryEnum Category{ get; set; }
+    [DisplayName("Tags")]
     public IEnumerable<string> Tags {get; set;}    
     public bool IsNSFW { get; set; }
 
